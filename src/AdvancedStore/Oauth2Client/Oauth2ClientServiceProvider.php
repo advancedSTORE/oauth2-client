db45eb4c6d5d75427b29ac70c80adca59e06dbbc
Basti <sk@advanced-store.com>
2014-04-03 11:04:14 +0200

    Project Start

---
+++ src/AdvancedStore/Oauth2Client/Oauth2ClientServiceProvider.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace AdvancedStore\Oauth2Client;
+
+use Illuminate\Support\ServiceProvider;
+
+class Oauth2ClientServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('advanced-store/oauth2-client');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
